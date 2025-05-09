6e293dadcec064765a4a625e5dbedefbf459419b
Guzmán Fernando Nahuel <nahuel1432@gmail.com>
2025-05-08 22:06:27 -0300

    first version

--- components/CalculatorButton.tsx
+++ components/CalculatorButton.tsx
@@ -1,24 +1,32 @@
-import { Colors } from '@/constants/Colors';
-import { globalStyles } from '@/style/global-styles';
-import * as Haptics from 'expo-haptics';
 import { Pressable, Text } from 'react-native';
 
+import * as Haptics from 'expo-haptics';
+
+import { Colors } from '@/constants/Colors';
+import { globalStyles } from '@/style/global-styles';
 
 interface Props {
-    label: string,
-    color?: string,
-    blackText?: boolean,
-    onPress: () => void,
-    dobleSize?: boolean,
+    label: string;
+    color?: string;
+    blackText?: boolean;
+    doubleSize?: boolean;
+    onPress: () => void;
 }
-const CalculatorButton = ({ label, color = Colors.darkGray, blackText = false, onPress, dobleSize = false }: Props) => {
+
+const CalculatorButton = ({
+    label,
+    color = Colors.darkGray,
+    blackText = false,
+    doubleSize = false,
+    onPress,
+}: Props) => {
     return (
         <Pressable
             style={({ pressed }) => ({
                 ...globalStyles.button,
                 backgroundColor: color,
                 opacity: pressed ? 0.8 : 1,
-                width: dobleSize ? 180 : 80
+                width: doubleSize ? 180 : 80,
             })}
             onPress={() => {
                 Haptics.selectionAsync();
@@ -26,15 +34,14 @@ const CalculatorButton = ({ label, color = Colors.darkGray, blackText = false, o
             }}
         >
             <Text
-                style={
-                    {
-                        ...globalStyles.buttonText,
-                        color: blackText ? 'black' : 'white'
-                    }
-                }
-            >{label}</Text>
+                style={{
+                    ...globalStyles.buttonText,
+                    color: blackText ? 'black' : 'white',
+                }}
+            >
+                {label}
+            </Text>
         </Pressable>
-    )
-}
-
-export default CalculatorButton
+    );
+};
+export default CalculatorButton;
